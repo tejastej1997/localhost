3a3e62551c9235f7cbe6b9c2a79e1095cf853b50
tejastej1997 <tejastej1997@gmail.com>
2023-03-02 17:02:19 +0530

    changes done

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ const jsonserver =require('json-server')
 const server = jsonserver.create()
 const route = jsonserver.router('db.json')
 const middleware =jsonserver.defaults()
-const cors = require("cors");
+const cors = require('cors');
 
 const port =process.env.PORT || 5000
 
 server.use(middleware)
 server.use(route)
-server.use(cors)
+server.use(cors())
 
 server.listen(port)
